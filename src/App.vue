38efd67b8e538df2bc69a6198a41c7cb8cda19b1
Jay <heng_huan@126.com>
2022-09-03 17:20:23 +0800

    feat: 初始化路由配置

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <router-view></router-view>
 </template>
 
 <style scoped>
